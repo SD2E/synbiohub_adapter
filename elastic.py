a6975bc8a2d89db96633d2dc9297371dc9b6e64e
Jed Singer <jed.singer@twosixlabs.com>
2018-02-16 20:21:06 +0000

    better unicode support, switch to sd2e user

--- elastic.py
+++ elastic.py
@@ -15,7 +15,7 @@ def virtuoso_to_elasticsearch_full(step=1000):
   sparql.setQuery("""
   SELECT COUNT DISTINCT *
   WHERE {{
-    GRAPH <http://hub.sd2e.org/user/sd2_service> {{ ?s [] ?o }}
+    GRAPH <http://hub.sd2e.org/user/sd2e> {{ ?s [] ?o }}
   }}""")
 
   sparql.setReturnFormat(JSON)
@@ -27,14 +27,14 @@ def virtuoso_to_elasticsearch_full(step=1000):
     sparql.setQuery("""
     SELECT DISTINCT *
     WHERE {{
-      GRAPH <http://hub.sd2e.org/user/sd2_service> {{ ?s [] ?o }}
+      GRAPH <http://hub.sd2e.org/user/sd2e> {{ ?s [] ?o }}
     }} LIMIT {} OFFSET {}""".format(step, offset))
 
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()['results']['bindings']
 
     def extract_uri_parts(uri):
-      parts = ["{}".format(p) for p in v['value'].split('/') if p and p not in {'http:', 'https:'}]
+      parts = [u"{}".format(p) for p in v['value'].split('/') if p and p not in {'http:', 'https:'}]
       bits = []
       for p in parts:
         bits.extend(p.split('_'))
@@ -43,7 +43,7 @@ def virtuoso_to_elasticsearch_full(step=1000):
     these_docs = []
     for r in results:
       for k, v in r.iteritems():
-        these_docs.append('{{ "{}": "{}", "parts": {} }}'.format(v['type'], v['value'],
+        these_docs.append(u'{{ "{}": "{}", "parts": {} }}'.format(v['type'], v['value'],
             json.dumps(extract_uri_parts(v['value']))))
 
     docs.update(these_docs)
@@ -54,7 +54,7 @@ def virtuoso_to_elasticsearch_full(step=1000):
 
   for d in docs:
     actions.append({
-      '_index': 'sd2_service',
+      '_index': 'sd2e',
       '_type': 'document',
       '_id': id,
       '_source': json.loads(d)
@@ -65,8 +65,8 @@ def virtuoso_to_elasticsearch_full(step=1000):
 
 
 def find_entities(input):
-  results = es.search(index='sd2_service', body={'query': {'multi_match': {'query': input, "fuzziness": "AUTO", 'fields': ['parts', 'uri', 'literal']}}})
-  results = set(['{{"{}": "{}"}}'.format(k, item['_source'][k]) for item in results['hits']['hits'] for k in item['_source'] if k != 'parts'])
+  results = es.search(index='sd2e', body={'query': {'multi_match': {'query': input, "fuzziness": "AUTO", 'fields': ['parts', 'uri', 'literal']}}})
+  results = set([u'{{"{}": "{}"}}'.format(k, item['_source'][k]) for item in results['hits']['hits'] for k in item['_source'] if k != 'parts'])
   results = [json.loads(s) for s in results]
   return results
 
