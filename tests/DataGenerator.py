4813df4ea9410a56b1cda21e983bd2ab497a2023
Tramy Nguyen (ADM) <tnguyen@bbn.com>
2018-04-06 16:56:30 -0400

    generated sbol files

---
+++ tests/DataGenerator.py
@@ -0,0 +1,33 @@
+
+from sbol import *
+import time
+from rdflib import Graph
+import re
+
+def get_uniqueID(idPrefix):
+	t = time.ctime()
+	uid = '_'.join([idPrefix, t])
+	return re.sub(r'[: ]', '_', uid)
+
+
+def create_compDef(idPrefix):
+	c_uri = get_uniqueID('compDef' + idPrefix)
+	compDef = ComponentDefinition(c_uri, BIOPAX_DNA, '1.0')
+	return compDef
+
+if __name__ == '__main__':
+	sbolDoc = Document()
+	setHomespace("https://www.bbn.com/")
+	for i in range(20000):
+		c = create_compDef(str(i))
+		sbolDoc.addComponentDefinition(c)
+
+	res = sbolDoc.writeString()
+	xmlGraph = Graph()
+	xmlGraph.parse(data=res)
+		
+	total_obj = []
+	for sbol_subj, sbol_pred, sbol_obj in xmlGraph:
+		total_obj.append(sbol_obj)
+	print(len(total_obj))
+	sbolDoc.write("examples/c_trips" + str(len(total_obj)) + ".xml")
