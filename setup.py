84b3c46c2d609e69f026eef528d526c1d6928fc0
nroehner <nicholasroehner@gmail.com>
2019-02-27 17:14:18 -0500

    Removed dependency on pysbolx and generate_sbol files.

--- setup.py
+++ setup.py
@@ -6,8 +6,5 @@ setup(
     name='synbiohub_adapter',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=['SPARQLWrapper', 'appdirs', 'requests', 'pySBOLx==0.1', 'pysbol'],
-    dependency_links=[
-        'git+https://git@github.com/nroehner/pySBOLx.git#egg=pySBOLx-0.1'
-    ]
+    install_requires=['SPARQLWrapper', 'appdirs', 'requests', 'pysbol']
 )
