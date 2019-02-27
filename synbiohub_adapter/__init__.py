8c0b405e1c49209f4c0571f9b30b0b3fe578f43b
nroehner <nicholasroehner@gmail.com>
2019-02-27 17:20:59 -0500

    Removed imports from synbiohub_adapter init file.

--- synbiohub_adapter/__init__.py
+++
@@ -1,9 +0,0 @@
-from synbiohub_adapter.SynBioHubUtil import (
-    SBOLConstants,
-    BBNConstants,
-    SD2Constants,
-    SBOLQuery
-)
-from synbiohub_adapter.query_synbiohub import (
-	SynBioHubQuery
-)
