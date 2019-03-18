c1f23b4e82716df6650d3f93b16776db29079bec
Tom Mitchell <tmitchel@bbn.com>
2019-03-18 13:55:24 -0400

    Bump version for development

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='synbiohub_adapter',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         'SPARQLWrapper>=1.8.2',
