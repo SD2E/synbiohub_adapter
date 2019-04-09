091950bf73ccca1f783969e26591c0c558ae647b
Nicholas Roehner <nroehner@ad.bbn.com>
2019-04-09 11:17:41 -0400

    Restored __init__.py files for synbiohubadapter and upload_sbol to previous state to avoid API-breaking changes.

--- synbiohub_adapter/__init__.py
+++ synbiohub_adapter/__init__.py
@@ -1,2 +1,12 @@
 # See https://www.python.org/dev/peps/pep-0440/ for info on version numbering
-__version__ = '1.1'
+__version__ = '2.1'
+
+from synbiohub_adapter.SynBioHubUtil import (
+    SBOLConstants,
+    BBNConstants,
+    SD2Constants,
+    SBOLQuery
+)
+from synbiohub_adapter.query_synbiohub import (
+    SynBioHubQuery
+)
